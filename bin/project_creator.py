1b14a81f04a0f8a46e4cac15811a0e41fa14820a
Ariel Calzada <ariel.calzada@gmail.com>
2018-08-20 22:42:19 -0500

    Added command to create django project

--- bin/project_creator.py
+++ bin/project_creator.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import os
+import subprocess
 import sys
 
 logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)s %(lineno)d %(funcName)s %(message)s",
@@ -12,6 +13,19 @@ logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)s %(lineno)d %(
 logger = logging.getLogger(__name__)
 
 
+def run_command(self, command):
+    """Run command using subprocess module."""
+    try:
+        logger.debug("Running: {}".format(command))
+        command_line_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        process_output, _ = command_line_process.communicate()
+
+        if command_line_process.returncode != 0:
+            raise Exception(process_output)
+    except Exception as e:
+        raise Exception(e)
+
+
 def main():
     parser = argparse.ArgumentParser(description="Creates a Django Project")
     parser.add_argument('location', help='Location for the new project, this must not exist.')
@@ -19,6 +33,14 @@ def main():
     args = parser.parse_args(sys.argv[1:])
     if os.path.exists(args.location):
         raise Exception("The location already exists")
+
+    command = [
+        "django-admin",
+        "startproject",
+        "conf",
+        args.location
+    ]
+    run_command(command)
 
 
 if __name__ == "__main__":
