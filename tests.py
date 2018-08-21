f11bbe8c15f3b4fb3483937e7a9384118fa03495
Ariel Calzada <ariel.calzada@gmail.com>
2018-08-20 22:08:41 -0500

    Added installation test

--- tests.py
+++ tests.py
@@ -1,5 +1,9 @@
 import logging
 import os
+import shutil
+import subprocess
+import tempfile
+import uuid
 import unittest
 
 logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)s %(lineno)d %(funcName)s %(message)s",
@@ -12,7 +16,33 @@ logger = logging.getLogger(__name__)
 class TestProjectCreator(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.temporal_directory = tempfile.mkdtemp()
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.exists(cls.temporal_directory):
+            shutil.rmtree(cls.temporal_directory)
+
+    def _run_command(self, command):
+        """Run command using subprocess module."""
+        try:
+            logger.debug("Running: {}".format(command))
+            command_line_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            process_output, _ = command_line_process.communicate()
+
+            if command_line_process.returncode != 0:
+                raise Exception(process_output)
+        except Exception as e:
+            raise Exception(e)
+
+    def _create_virtualenv(self):
+        """Create random virtualenv."""
+        name = uuid.uuid4().hex
+        path = os.path.join(self.temporal_directory, name)
+        command = ["virtualenv", path]
+        self._run_command(command)
+
+        return path
 
     def test_no_location(self):
         """No location provided."""
@@ -24,4 +54,8 @@ class TestProjectCreator(unittest.TestCase):
 
     def test_valid_location(self):
         """Valid location."""
-        pass
+        path = self._create_virtualenv()
+        python_executable = os.path.join(path, "bin", "python")
+        pip_executable = os.path.join(path, "bin", "pip")
+        command = [pip_executable, "install", "git+https://github.com/arielcalzadadeveloper/django-project-creator.git"]
+        self._run_command(command)
