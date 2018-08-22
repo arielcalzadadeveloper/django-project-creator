7e1f4b10621f6c6d50aecb7bd9cde966f30e6f51
Ariel Calzada <ariel.calzada@gmail.com>
2018-08-22 14:04:19 -0500

    Replaced quotes

--- bin/project_creator.py
+++ bin/project_creator.py
@@ -273,11 +273,11 @@ ACCOUNT_USERNAME_VALIDATORS = None
         # Static/Media files
         old_string = "STATIC_URL = '/static/'"
         new_string = """STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_URL = "/base_project_static/"
+STATIC_URL = '/base_project_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/base_project_media/"""
+MEDIA_URL = '/base_project_media/'"""
         contents = contents.replace(old_string, new_string)
 
         # Last sections
