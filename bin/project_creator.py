793ce3dcec652567b257a612aa421585b8655179
Ariel Calzada <ariel.calzada@gmail.com>
2018-08-22 06:50:29 -0500

    Added modification of urls and wsgi

--- bin/project_creator.py
+++ bin/project_creator.py
@@ -15,6 +15,8 @@ class ProjectCreator:
         self._create_project(location)
         self._get_base_project(location)
         self._setup_environment_file(location)
+        self._modify_wsgi_file(location)
+        self._modify_urls_file(location)
 
     @staticmethod
     def _run_command(command, with_subprocess=True):
@@ -82,6 +84,61 @@ class ProjectCreator:
         target = os.path.join(location, ".env")
         command = ["mv", "-f", source, target]
         self._run_command(command, False)
+
+    def _modify_wsgi_file(self, location):
+        path = os.path.join(location, "conf", "wsgi.py")
+
+        with open(path, "r") as fh:
+            contents = fh.read()
+
+        old_string = "import os"
+        new_string = "{}\nimport dotenv".format(old_string)
+        contents = contents.replace(old_string, new_string)
+
+        old_string = "from django.core.wsgi import get_wsgi_application"
+        new_string = "{}\ndotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))".format(
+            old_string)
+        contents = contents.replace(old_string, new_string)
+
+        with open(path, "w") as fh:
+            fh.write(contents)
+
+    def _modify_urls_file(self, location):
+        path = os.path.join(location, "conf", "urls.py")
+
+        with open(path, "r") as fh:
+            contents = fh.read()
+
+        # Add imports
+        new_imports = [
+            "import allauth",
+            "import dynamic_raw_id",
+        ]
+        old_string = "from django.urls import path"
+        new_string = "{}\n\n{}".format(old_string, "\n".join(new_imports))
+        contents = contents.replace(old_string, new_string)
+
+        # Add patterns
+        new_patterns = [
+            "path('accounts/', allauth.urls),",
+            "path('admin/dynamic_raw_id/', dynamic_raw_id.urls),",
+        ]
+        old_string = "    path('admin/', admin.site.urls),"
+        new_string = "{}\n\n{}".format(old_string, "\n".join(new_patterns))
+        contents = contents.replace(old_string, new_string)
+
+        # Add media access if DEBUG
+        contents += "\n"
+        contents += "if settings.DEBUG:\n"
+        contents += "    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)"
+
+        # Add admin titles
+        contents += "\n"
+        contents += "admin.site.site_header = 'Base Project'\n"
+        contents += "admin.site.index_header = 'Base Project'"
+
+        with open(path, "w") as fh:
+            fh.write(contents)
 
 
 def main():
