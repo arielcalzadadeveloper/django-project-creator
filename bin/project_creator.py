943db6688ad2a31f522efc75298c8de708ca2858
Ariel Calzada <ariel.calzada@gmail.com>
2018-08-22 14:47:17 -0500

    Fixed indentation

--- bin/project_creator.py
+++ bin/project_creator.py
@@ -134,9 +134,9 @@ class ProjectCreator:
 
         # Add patterns
         new_patterns = [
-            "     url(r'^accounts/', include('allauth.urls')),,",
-            "     url(r'^admin/dynamic_raw_id/', include('dynamic_raw_id.urls')),",
-            "     url(r'^$', TemplateView.as_view(template_name=home_template), name='home'),",
+            "    url(r'^accounts/', include('allauth.urls')),,",
+            "    url(r'^admin/dynamic_raw_id/', include('dynamic_raw_id.urls')),",
+            "    url(r'^$', TemplateView.as_view(template_name=home_template), name='home'),",
         ]
         old_string = "    path('admin/', admin.site.urls),"
         new_string = "{}\n\n{}".format(old_string, "\n".join(new_patterns))
